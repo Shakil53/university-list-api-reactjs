56c85cfce96f23e8f37cea9d801244fafe938d6d
Shakil53 <shakilahmed.dcc@gmail.com>
2022-12-04 00:43:00 +0600

    university with its css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UniversityList></UniversityList>
     </div>
   );
 }
+
+function UniversityList() {
+  const [universities, setUniversity] = useState([])
+  useEffect(() => {
+    fetch('http://universities.hipolabs.com/search?country=United+States')
+      .then(res => res.json())
+    .then(data => setUniversity(data))
+  },[])
+
+
+  return (
+    <div>
+      <h1>Total University: {universities.length} </h1>
+      <div className='university'>
+      {
+        universities.map(university => <University name={university.name} country={university.country} webPage={university.web_pages[0]}></University>)
+      }
+      </div>
+    </div>
+  )
+}
+
+function University(props) {
+  return (
+    <div className='single-uni'>
+      <h3>Name: {props.name}</h3>
+      <h4>Country: {props.country}</h4>
+      <p>Website: {props.webPage}</p>
+    </div>
+  )
+}
+
 
 export default App;
